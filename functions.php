5f06d48e0d13674b1b1ca72a48c4551d03c5f751
erinrezner <erinrezner@gmail.com>
2020-09-03 00:01:25 -0400

    Delete

--- functions.php
+++ functions.php
@@ -1,38 +1,12 @@
 <?php
-//Remove Image Compression
-add_filter('jpeg_quality', create_function('', 'return 100;'));
-add_filter('wp_editor_set_quality', create_function('', 'return 100;'));
+//Set Upload Sizes
+@ini_set( 'upload_max_size' , '128M' );
+@ini_set( 'post_max_size', '32M');
+@ini_set( 'max_execution_time', '300' );
 
 //Title
 add_theme_support( 'title-tag' );
 
 //Remove WP Generator Meta Tag
 remove_action('wp_head', 'wp_generator');
-
-// No Self Pings
-function no_self_ping( &$links ) {
-	$home = get_option( 'home' );
-	foreach ( $links as $l => $link )
-		if ( 0 === strpos( $link, $home ) )
-			unset($links[$l]);
-}
-add_action( 'pre_ping', 'no_self_ping' );
-
-//* Change the URL of the WordPress login logo
-function TR_url_login_logo()
-{
-  return site_url();;
-}
-add_filter('login_headerurl', 'TR_url_login_logo');
-//* Login Screen: Set 'remember me' to be checked
-function TR_login_checked_remember_me()
-{
-  add_filter( 'login_footer', 'TR_rememberme_checked' )
-  ;
-}
-function TR_rememberme_checked()
-{
-  echo "<script>document.getElementById('rememberme').checked = true;</script>";
-}
-add_action( 'init', 'TR_login_checked_remember_me' );
 ?>
